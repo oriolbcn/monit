0c5d99e455760d073b7e23925c516b31afc34c4c
Oriol Collell <oriol.collell.bcn@gmail.com>
2014-01-11 19:22:34 +0100

    added additional allow statement

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name              "monit-tlq"
 maintainer        "Oriol Collell"
 maintainer_email  "ocollell@dineyo.com"
 description       "Basic monit installation"
-version           "0.3.11"
+version           "0.3.12"
 
 supports      "ubuntu"
