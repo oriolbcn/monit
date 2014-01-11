3e4ce70bc7832a5d5d1200e64fef2f48a702e1e6
Oriol Collell <oriol.collell.bcn@gmail.com>
2014-01-11 16:55:52 +0100

    added dynamic httpd address

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name              "monit-tlq"
 maintainer        "Oriol Collell"
 maintainer_email  "ocollell@dineyo.com"
 description       "Basic monit installation"
-version           "0.3.10"
+version           "0.3.11"
 
 supports      "ubuntu"
