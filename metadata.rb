05b248bca147fb82e4e4d64e6b681c743583e699
Oriol Collell <oriol.collell.bcn@gmail.com>
2014-01-26 17:52:13 +0100

    more alrets ignored

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name              "monit-tlq"
 maintainer        "Oriol Collell"
 maintainer_email  "ocollell@dineyo.com"
 description       "Basic monit installation"
-version           "0.3.12"
+version           "0.3.13"
 
 supports      "ubuntu"
